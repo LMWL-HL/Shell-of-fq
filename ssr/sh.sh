096ccbd8df1fe2c3f089bf62776e89084b21df68
hulan <lmwl2hl@gmail.com>
2019-01-14 14:06:08 +0800

    fixed

--- ssr/sh.sh
+++ ssr/sh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.6
 import os
 
-cmd = ["wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh","chmod +x shadowsocksR.sh","./shadowsocksR.sh 2>&1 | tee shadowsocksR.log","wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh","chmod +x bbr.sh","./bbr.sh"]
+cmd = ["wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh","chmod +x shadowsocksR.sh","./shadowsocksR.sh 2>&1 | tee shadowsocksR.log","cp shadowsocks.json /etc/shadowsocks.json","wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh","chmod +x bbr.sh","./bbr.sh"]
 
 for cm in cmd:
 	os.system(cm)
