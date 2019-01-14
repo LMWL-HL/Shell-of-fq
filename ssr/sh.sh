9f51fb0abf4dcd9d6a6ff763da737a0bccfbf4c7
hulan <lmwl2hl@gmail.com>
2019-01-14 16:49:51 +0800

    fixed the python version

--- ssr/sh.sh
+++ ssr/sh.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3.5
 import os
 
 cmd = ["wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh","chmod +x shadowsocksR.sh","./shadowsocksR.sh 2>&1 | tee shadowsocksR.log","cp shadowsocks.json /etc/shadowsocks.json","wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh","chmod +x bbr.sh","./bbr.sh"]
