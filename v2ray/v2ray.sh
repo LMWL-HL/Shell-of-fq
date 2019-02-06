1d1d7dacd831c4b6dbca9e47c89eee2a81297fc0
hulan <lmwl2hl@gmail.com>
2019-02-06 14:14:08 +0800

    stupid bug

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -2,7 +2,7 @@
 wget https://install.direct/go.sh
 bash go.sh
 python3 service_user.py
-cp config.json /etc/v2ray/config.json
+cp config_vps.json /etc/v2ray/config.json
 systemctl start v2ray
 wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 chmod +x bbr.sh
