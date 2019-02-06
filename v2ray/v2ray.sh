728b0ad55c4e384db7a3e52a509683ce2b0d9552
hulan <lmwl2hl@gmail.com>
2019-02-06 14:08:33 +0800

    fixed forgot

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 wget https://install.direct/go.sh
 bash go.sh
-python3 user.py
+python3 service_user.py
 cp config.json /etc/v2ray/config.json
 systemctl start v2ray
 wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
