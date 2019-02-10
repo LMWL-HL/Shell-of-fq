6321ea22916bc0c37876ac6e1cb48172893be795
hulan <lmwl2hl@gmail.com>
2019-02-10 17:52:22 +0800

    fixed

--- v2ray/user_operate.py
+++ v2ray/user_operate.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-def add_usr(n)
+def add_usr(n):
     i = os.popen("cat /proc/sys/kernel/random/uuid")
     global yid
     yid = 0
@@ -24,28 +24,28 @@ def add_usr(n)
     print("\tid:\0" + yid)
     print("\tport:\0" + str(load_dict["inbounds"][0]["port"]))
     print("\tprotocol:\0" + str(load_dict["inbounds"][0]["protocol"]))
-    print("\tlevel:\0" + str(load_dict["inbounds"][0]["settings"]["client"][0]["level"]))
-    print("\talterId:\0" + str(load_dict["inbounds"][0]["settings"]["client"][0]["alterId"]))
+    print("\tlevel:\0" + str(load_dict["inbounds"][0]["settings"]["clients"][0]["level"]))
+    print("\talterId:\0" + str(load_dict["inbounds"][0]["settings"]["clients"][0]["alterId"]))
 
 def remove_user(dic):
     load_f = open("/etc/v2ray/config.json","r",encoding="utf-8")
     load_dict = json.load(load_f)
-    if sorted(dic)[0]-1 <= load_dict["inbounds"][0]["settings"]["clients"]:
+    if int(sorted(dic)[-1])-1 <= load_dict["inbounds"][0]["settings"]["clients"]:
         load_f.close()
     else:
         return "超出用户数量范围！"
     for i in dic:
+        print("已删除用户：")
+        print("\tid:\0" + str(load_dict["inbounds"][0]["settings"]["clients"][int(i)-1]["id"]))
         load_f = open("/etc/v2ray/config.json","r",encoding="utf-8")
         load_dict = json.load(load_f)
-        del load_dict["inbounds"][0]["settings"]["clients"][str(i)]
+        del load_dict["inbounds"][0]["settings"]["clients"][int(i)-1]
         load_f.close()
 
-        load_f = open("/etc/v2ray/config.json","r",encoding="utf-8")
+        load_f = open("/etc/v2ray/config.json","w",encoding="utf-8")
         json.dump(load_dict,load_f)
-        load_f.close()
-        print("已删除用户：")
-        print("\tid:\0" + str(load_dict["inbounds"][0]["settings"]["client"][0]["id"]))
-        return "Done!"
+        load_f.close()       
+    return "Done!"
 
 while True:
     try:
@@ -61,7 +61,7 @@ while True:
 if op == 1:
     n = input("添加多少位用户：")
 
-    for i in range(n):
+    for i in range(str(n)):
         add_usr(i)
 else:
     l = input("要删除那些用户（如1，2，3）：\0")
@@ -71,4 +71,5 @@ else:
             test = int(i)
         except Exception as e:
             print("请输入数字！")
-    print(remove_user(li))
+    info = remove_user(li)
+    print(info)
