bb40d93c3b9c14f4c0afca00407bbd2c08c4d791
hulan <lmwl2hl@gmail.com>
2019-09-02 20:54:40 +0800

    all to sh

---
+++ v2ray/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 输入你的域名：
+read hosts
+echo $hosts
--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 wget https://install.direct/go.sh
 bash go.sh
-python3 service_user.py
-cp config_vps.json /etc/v2ray/config.json
+echo 输入你的域名：
+read host
+apt install socat
+curl https://get.acme.sh | sh
+source ~/.bashrc
+~/.acme.sh/acme.sh --issue -d $host --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $host --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+uuid=`cat /proc/sys/kernel/random/uuid`
+sed -i "s/HostReplace/$host/g" config.json
+sed -i "s/IdReplace/$uuid/g" config.json
+cp config.json /etc/v2ray/config.json
 systemctl start v2ray
 wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 chmod +x bbr.sh
