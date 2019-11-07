88e8825d0a8b55f37f04db1e7085b072477ece0b
hulan <lmwl2hl@gmail.com>
2019-11-07 14:35:37 +0800

    fixed v2ray.sh

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -10,9 +10,16 @@ source ~/.bashrc
 ~/.acme.sh/acme.sh --installcert -d $host --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 uuid=`cat /proc/sys/kernel/random/uuid`
 sed -i "s/HostReplace/$host/g" config.json
+sed -i "s/IdReplace/$uuid/g" config_vps.json
 sed -i "s/IdReplace/$uuid/g" config.json
-cp config.json /etc/v2ray/config.json
+cp config_vps.json /etc/v2ray/config.json
 systemctl start v2ray
-wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
-chmod +x bbr.sh
-./bbr.sh
+bbr_install=`lsmod | grep bbr`
+just=`expr index $bbr_install "tcp_bbr"`
+if [ $just -ne 0 ];then
+  echo deployed
+else
+  wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+  chmod +x bbr.sh
+  ./bbr.sh
+fi
