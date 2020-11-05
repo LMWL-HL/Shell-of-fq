86dab0d9922e337927a9eccaadc59a0ab2f4021a
HuLan <lmwl2hl@gmail.com>
2020-11-05 21:12:11 +0800

    nothing

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -1,61 +1,79 @@
 #!/bin/bash
 
-# read info.
-echo "输入你的域名："
-read HOST
 UUID=`cat /proc/sys/kernel/random/uuid`
 PORT=$((RANDOM + 1024))
+BBRURL="https://github.com/teddysun/across/raw/master/bbr.sh"
+V2RAYURL="https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh"
+CERTPATH="/etc/letsencrypt/live/"
+V2RAYCERTPATH="/etc/ssl/v2ray/"
+SHCONTENT="#!/bin/bash\n\nV2RAY_DOMAIN='$HOST'\n\nif [[ \"\$RENEWED_LINEAGE\" == \"$CERTPATH/\$V2RAY_DOMAIN\" ]]; then\n\tinstall -m 644 -o root -g root \"$CERTPATH/\$V2RAY_DOMAIN/fullchain.pem\" -t $V2RAYCERTPATH\n\tinstall -m 644 -o root -g root \"$CERTPATH/\$V2RAY_DOMAIN/privkey.pem\" -t $V2RAYCERTPATH\n\n\tsleep \"\$((RANDOM % 2048))\"\n\tsystemctl restart v2ray.service\nfi\n"
+
+# read info.
+read_info(){
+    echo "输入你的域名："
+    read HOST
+}
 
 # v2ray install
-bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
-if [[ $? != 0 ]]; then
-  echo "v2ray install failed!"
-  exit 1
-fi
+install_v2ray(){
+    bash <(curl -L $V2RAYURL)
+    if [[ $? != 0 ]]; then
+        echo "v2ray install failed!"
+        exit 1
+    fi
+}
 
 # install cert file.
-apt install certbot
-certbot certonly --standalone --agree-tos --email lmwl2hl@gmail.com --no-eff-email --domains $HOST
-if [[ $? != 0 ]]; then
-  echo "证书安装失败!"
-  exit 1;
-fi
-install -d -o root -g root /etc/ssl/v2ray/
-install -m 644 -o root -g root /etc/letsencrypt/live/$HOST/fullchain.pem -t /etc/ssl/v2ray/
-install -m 644 -o root -g root /etc/letsencrypt/live/$HOST/privkey.pem -t /etc/ssl/v2ray/
-cat > /etc/letsencrypt/renewal-hooks/deploy/v2ray.sh << EOF
-#!/bin/bash
-
-V2RAY_DOMAIN='$HOST'
-
-if [[ "\$RENEWED_LINEAGE" == "/etc/letsencrypt/live/\$V2RAY_DOMAIN" ]]; then
-    install -m 644 -o root -g root "/etc/letsencrypt/live/\$V2RAY_DOMAIN/fullchain.pem" -t /etc/ssl/v2ray/
-    install -m 644 -o root -g root "/etc/letsencrypt/live/\$V2RAY_DOMAIN/privkey.pem" -t /etc/ssl/v2ray/
-
-    sleep "\$((RANDOM % 2048))"
-    systemctl restart v2ray.service
-fi
-EOF
-chmod +x /etc/letsencrypt/renewal-hooks/deploy/v2ray.sh
+install_cert_file(){
+    apt install certbot
+    certbot certonly --standalone --agree-tos --email yourmail@gmail.com --no-eff-email --domains $HOST
+    if [[ $? != 0 ]]; then
+        echo "证书安装失败!"
+        exit 1;
+    fi
+    install -d -o root -g root $V2RAYCERTPATH
+    install -m 644 -o root -g root $CERTPATH/$HOST/fullchain.pem -t $V2RAYCERTPATH
+    install -m 644 -o root -g root $CERTPATH/$HOST/privkey.pem -t $V2RAYCERTPATH
+    cp deploy.sh /etc/letsencrypt/renewal-hooks/deploy/v2ray.sh
+    echo $SHCONTENT > /etc/letsencrypt/renewal-hooks/deploy/v2ray.sh
+    chmod +x /etc/letsencrypt/renewal-hooks/deploy/v2ray.sh
+}
 
 # modify client.json and server.json
-sed -i "s/HostReplace/$HOST/g" client.json
-sed -i "s/\"PortReplace\"/$PORT/g" client.json
-sed -i "s/IdReplace/$UUID/g" client.json
+modify_config(){
+    sed -i "s/HostReplace/$HOST/g" client.json
+    sed -i "s/\"PortReplace\"/$PORT/g" client.json
+    sed -i "s/IdReplace/$UUID/g" client.json
 
-sed -i "s/HostReplace/$HOST/g" server.json
-sed -i "s/\"PortReplace\"/$PORT/g" server.json
-sed -i "s/IdReplace/$UUID/g" server.json
-cp server.json /usr/local/etc/v2ray/config.json
+    sed -i "s/HostReplace/$HOST/g" server.json
+    sed -i "s/\"PortReplace\"/$PORT/g" server.json
+    sed -i "s/IdReplace/$UUID/g" server.json
+    cp server.json /usr/local/etc/v2ray/config.json
+}
 
 # check bbr installed or not.
-lsmod | grep bbr > /dev/null
-if [[ $? != 0 ]]; then
-  bash <(wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh)
-else
-  echo "bbr already installed!"
-fi
+check_and_install_bbr(){
+    lsmod | grep bbr > /dev/null
+    if [[ $? != 0 ]]; then
+        bash <(wget --no-check-certificate $BBRURL)
+    else
+        echo "bbr already installed!"
+    fi
+}
 
 # start v2ray.service
-systemctl enable v2ray.service
-systemctl start v2ray.service
+start_v2ray(){
+    systemctl enable v2ray.service
+    systemctl start v2ray.service
+}
+
+main(){
+    read_info
+    install_v2ray
+    install_cert_file
+    modify_config
+    check_and_install_bbr
+    start_v2ray
+}
+
+main "$@"
